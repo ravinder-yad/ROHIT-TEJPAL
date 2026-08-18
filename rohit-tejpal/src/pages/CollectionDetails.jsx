165f4971f0f18b02dd3d092e16a3a7c48fd75f24
Ravinder Yadav <ravinderyadav092007@gmail.com>
2026-08-18 10:29:54 +0530

    Fix collection banner image crop by anchoring it to top

--- rohit-tejpal/src/pages/CollectionDetails.jsx
+++ rohit-tejpal/src/pages/CollectionDetails.jsx
@@ -86,7 +86,7 @@ const CollectionDetails = () => {
         {/* Background Image if available */}
         {collection.image && (
           <div className="absolute inset-0 z-0">
-            <img src={collection.image} alt={collection.name} className="w-full h-full object-cover opacity-20" />
+            <img src={collection.image} alt={collection.name} className="w-full h-full object-cover object-top opacity-20" />
             <div className="absolute inset-0 bg-gradient-to-t from-[var(--color-primary-dark)] to-transparent"></div>
           </div>
         )}
